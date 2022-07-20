a60fa9a0dbd0e3519e668d61f3fdcd0aab3ff416
Shubham-Srivastava02_lmig <--global>
2022-07-21 04:38:23 +0530

    removed colun from exception pattern

--- src/main/java/com/lmig/libertyconnect/sms/stack/utils/Constants.java
+++ src/main/java/com/lmig/libertyconnect/sms/stack/utils/Constants.java
@@ -32,5 +32,5 @@ public class Constants {
     public static final String PROD_SECRET_ID = "";
 
     // Used by alarm metric
-    public static final String ERROR_PREFIX = "LCException:";
+    public static final String ERROR_PREFIX = "LCException";
 }
